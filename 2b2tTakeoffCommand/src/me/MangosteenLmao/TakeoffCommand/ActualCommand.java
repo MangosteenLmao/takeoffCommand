1fd74d8a0efab0aaf92d53244d74f9dd1c1a2a27
MangosteenLmao <26844143+MangosteenLmao@users.noreply.github.com>
2020-05-31 16:43:43 -0400

    xp, glide mode
    added xp sound effect, added glide mode. Glide mode is slightly glitchy, sorry

--- 2b2tTakeoffCommand/src/me/MangosteenLmao/TakeoffCommand/ActualCommand.java
+++ 2b2tTakeoffCommand/src/me/MangosteenLmao/TakeoffCommand/ActualCommand.java
@@ -1,6 +1,7 @@
 package me.MangosteenLmao.TakeoffCommand;
 
 import org.bukkit.entity.Entity;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -14,30 +15,38 @@ public class ActualCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (label.equalsIgnoreCase("takeoff")) {
 			if (sender instanceof Player) {
-				if (((Player) sender).getInventory().getChestplate().getType() == null) {
-					sender.sendMessage("You need to be wearing an elytra!");
+				if (((Player) sender).getInventory().getChestplate() == null) {
+					sender.sendMessage(net.md_5.bungee.api.ChatColor.RED + "You need to be wearing an elytra!");
 				}
+				try {
 					if (((Player) sender).getInventory().getChestplate().getType() == Material.ELYTRA) {
 						if (((Entity) sender).isOnGround()) {
 							Location location = ((Player) sender).getLocation();
 							Location checkBlock = location;
-							if (checkBlock.getBlock().getType() == Material.AIR && checkBlock.add(0, 1, 0).getBlock().getType() == Material.AIR && checkBlock.add(0, 2, 0).getBlock().getType() == Material.AIR) {
-								((Player) sender).teleport(location);
+							if (checkBlock.add(0, 1, 0).getBlock().getType() == Material.AIR && 
+								checkBlock.add(0, 2, 0).getBlock().getType() == Material.AIR) {
+								((Player) sender).teleport(location.add(0, 1, 0));
+								((Player) sender).setGliding(true);
+								((Player) sender).playSound(location, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1F, 10F);
 								
 							} else {
-								sender.sendMessage("You need to have two blocks of air above you!");
+								sender.sendMessage(net.md_5.bungee.api.ChatColor.RED + "You need to have two blocks of air above you!");
 							}
 						} else {
-							sender.sendMessage("You need to be standing on a block!");
+							sender.sendMessage(net.md_5.bungee.api.ChatColor.RED + "You need to be standing on a block!");
 						}
 					} else {
-						sender.sendMessage("You need to be wearing an elytra!");
+						sender.sendMessage(net.md_5.bungee.api.ChatColor.RED + "You need to be wearing an elytra!");
 					}
+				} catch (NullPointerException e) {
+					e.printStackTrace();
+				}
 			} else {
-				sender.sendMessage("You are not a player!");
+				sender.sendMessage(net.md_5.bungee.api.ChatColor.RED + "You are not a player!");
 				return true;
 			}
 		}
 		return false;
 	}
+	
 }
