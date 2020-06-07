6a539e6b51a4a3dcee34dcd0ac456f9372e52419
MangosteenLmao <26844143+MangosteenLmao@users.noreply.github.com>
2020-06-07 08:52:03 -0400

    moved command to main, fixed gliding issue
    Moved the actualcommand class content to here, added a short delay before setting the player to "glide" mode to fix takeoff issue.

--- 2b2tTakeoffCommand/src/me/MangosteenLmao/TakeoffCommand/TakeoffCommand.java
+++ 2b2tTakeoffCommand/src/me/MangosteenLmao/TakeoffCommand/TakeoffCommand.java
@@ -3,12 +3,15 @@ package me.MangosteenLmao.TakeoffCommand;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Bukkit;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.craftbukkit.Main;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 
-import me.MangosteenLmao.TakeoffCommand.ActualCommand;
 
-@SuppressWarnings("unused")
 public class TakeoffCommand extends JavaPlugin implements Listener {
 
 	public static TakeoffCommand plugin;
@@ -17,7 +20,46 @@ public class TakeoffCommand extends JavaPlugin implements Listener {
 	public void onEnable() {
 		plugin = this;
 		Bukkit.getServer().getPluginManager().registerEvents(this, this);
-		this.getCommand("takeoff").setExecutor((CommandExecutor)new ActualCommand());
 	}
-	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (label.equalsIgnoreCase("takeoff")) {
+			if (sender instanceof Player) {
+				if (((Player) sender).getInventory().getChestplate() == null) {
+					sender.sendMessage(net.md_5.bungee.api.ChatColor.RED + "You need to be wearing an elytra!");
+				}
+				try {
+					if (((Player) sender).getInventory().getChestplate().getType() == Material.ELYTRA) {
+						if (((Entity) sender).isOnGround()) {
+							Location location = ((Player) sender).getLocation();
+							Location checkBlock = location;
+							if (checkBlock.add(0, 1, 0).getBlock().getType() == Material.AIR && 
+								checkBlock.add(0, 2, 0).getBlock().getType() == Material.AIR) {
+								((Player) sender).teleport(location.add(0, 1, 0));
+								((Player) sender).playSound(location, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1F, 10F);
+								Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() { //delays gliding
+									public void run() {
+										((Player) sender).setGliding(true);
+									}
+								}, 5L);
+								//hi hause lol
+							} else {
+								sender.sendMessage(net.md_5.bungee.api.ChatColor.RED + "You need to have two blocks of air above you!");
+							}
+						} else {
+							sender.sendMessage(net.md_5.bungee.api.ChatColor.RED + "You need to be standing on a block!");
+						}
+					} else {
+						sender.sendMessage(net.md_5.bungee.api.ChatColor.RED + "You need to be wearing an elytra!");
+					}
+				} catch (NullPointerException e) {
+					e.printStackTrace();
+				}
+			} else {
+				sender.sendMessage(net.md_5.bungee.api.ChatColor.RED + "You are not a player!");
+				return true;
+			}
+		}
+		return false;
+	}
 }
